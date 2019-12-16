b62231a77a2edde49ac3bd67af834880b63d498f
Oleg <jf2.oleg@gmail.com>
2019-12-16 20:59:25 +0200

    Добавили отображения списка городов и детализацию по городу

--- cities/models.py
+++ cities/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 class City(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название города')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import City
 
-# Create your views here.
+def home(request, pk=None):
+    if pk:
+        city = City.objects.filter(id=pk).first()
+        return render(request, 'cities/detail.html', {'object': city, })
+    cities = City.objects.all()
+    return render(request, 'cities/home.html', {'objects_list': cities, })
+
